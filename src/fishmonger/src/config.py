dd013b89cfad961c1bbc31f14a0b41ddac4a260b
Charles Zilm <Charles Zilm>
2015-12-27 15:06:46 -0500

    Checkpoint
    Checkpoint

--- src/fishmonger/src/config.py
+++ src/fishmonger/src/config.py
@@ -6,3 +6,51 @@ import pybase.log as PyLog
 
 ## Fishmonger modules included
 import fishmonger.exceptions as FishExc
+
+"""
+Config
+
+Four sources
+
+CLI
+ENV
+.fishmonger file at project root
+.fishmonger file at source  root
+
+CLI overrides environment
+ENV overrides file
+SRC file overrides root
+ROOT file overrides defaults
+"""
+
+class Config():
+	def __init__(self):
+		self.configs  = []
+		self.values   = {}
+
+	def __getitem__(self, key):
+		if key in self.values:
+			return self.values[key]
+		else:
+			for (t, config) in self.configs:
+				if key in config:
+					return config[key]
+
+	def __setitem__(self, key, value):
+		self.value[key] = value
+
+	def get(self, key, default=(None, False)):
+		try:
+			return self.__getitem__(key)
+		except Exception as e:
+			if default == (None, False):
+				raise(e)
+			return default
+
+	def set(self, key, value):
+		self.__setitem__(key, value)
+
+	def addConfig(self, config, priority=None):
+		priority = priority if priority != None else len(self.configs)
+		self.configs.append((priority, config))
+		self.configs.sort()
